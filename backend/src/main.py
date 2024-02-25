c8653358b9b6712e0962eb5d2eaf2982f6dc6673
rennySPSUL <55239110+martinrenner@users.noreply.github.com>
2024-02-25 17:14:49 +0100

    Komunikace frontend a backend funkcni

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost:8000", "http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"], max_age=600
+)
 
 
 @app.get("/")
